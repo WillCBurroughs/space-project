7c685e746d55c72e7cc0183a1d13e55819b076ed
William <willcburroughs@gmail.com>
2024-11-17 13:23:03 -0600

    Feature Development: Positioning home better

--- Space_Project Shared/HomeScreen.swift
+++ Space_Project Shared/HomeScreen.swift
@@ -17,7 +17,7 @@ class HomeScreen: SKScene {
     override func didMove(to view: SKView) {
         
         // Add the menu display
-        menuDisplay.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        menuDisplay.position = CGPoint(x: size.width / 2, y: size.height * 0.6)
         menuDisplay.size = CGSize(width: self.size.width, height: self.size.height)
         self.addChild(menuDisplay)
         
@@ -38,8 +38,8 @@ class HomeScreen: SKScene {
         
         // "Levels" button
         let levelsButton = SKShapeNode(rectOf: CGSize(width: buttonWidth, height: buttonHeight), cornerRadius: 10)
-        levelsButton.position = CGPoint(x: size.width / 2, y: size.height * 0.37)  // Position it over the "Levels" button area in the image
-        levelsButton.fillColor = .clear  // Make it invisible or semi-transparent for debugging
+        levelsButton.position = CGPoint(x: size.width / 2, y: size.height * 0.47)  // Position it over the "Levels" button area in the image
+        levelsButton.fillColor = .clear // Make it invisible or semi-transparent for debugging
         levelsButton.zPosition = 10
         levelsButton.strokeColor = .clear
         levelsButton.name = "levelsButton"
@@ -47,7 +47,7 @@ class HomeScreen: SKScene {
         
         // "Shop" button
         let shopButton = SKShapeNode(rectOf: CGSize(width: buttonWidth, height: buttonHeight), cornerRadius: 10)
-        shopButton.position = CGPoint(x: size.width / 2, y: size.height * 0.2)  // Position it over the "Shop" button area in the image
+        shopButton.position = CGPoint(x: size.width / 2, y: size.height * 0.3)  // Position it over the "Shop" button area in the image
         shopButton.fillColor = .clear  // Make it invisible or semi-transparent for debugging
         shopButton.strokeColor = .clear
         shopButton.zPosition = 10
